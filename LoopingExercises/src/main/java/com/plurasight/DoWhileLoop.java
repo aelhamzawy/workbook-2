184bdbf6c97d4d1784dce330caa2b5451fb1c55d
Ahmed Elhamzawy <aelhamzawy2020@gmail.com>
2025-04-21 10:09:21 -0800

    creating the do while loop

--- LoopingExercises/src/main/java/com/plurasight/DoWhileLoop.java
+++ LoopingExercises/src/main/java/com/plurasight/DoWhileLoop.java
@@ -1,4 +1,21 @@
 package com.plurasight;
 
+
+
 public class DoWhileLoop {
-}
+
+    public static void main(String[] args){
+
+        printNumber(0,4,"I Love Java");
+    }
+
+    public static void printNumber(int counter, int printNumber, String text){
+        do{
+            System.out.println(text);
+            counter++;
+        }
+        while(counter < printNumber);
+    }
+};
+
+
